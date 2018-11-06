b9a09c2e0161f4af69ffae4f1e37953ceb2c58f4
Matthew Grove <matthew@grove.gq>
2018-11-06 19:40:25 +0000

    Add text to centre of doughnut charts

--- assets/doughnut_charts.js
+++ assets/doughnut_charts.js
@@ -23,13 +23,62 @@ dataTimePeriod is:
     Today
     Past Week
     Past Month
-
-dataTimeFrame is:
-    day
-    week
-    month
 */
-function drawDoughnutChart(chartLocationID,csvFileLocation,dataTimePeriod,dataTimeFrame) {
+function drawDoughnutChart(chartLocationID,csvFileLocation,dataTimePeriod) {
+    var dataTimeFrame = ""
+    var doughnutChartCenterLabel = ""
+    switch(dataTimePeriod) {
+        case "Today":
+            dataTimeFrame = "day";
+            doughnutChartCenterLabel = "Today";
+        case "Past Week":
+            dataTimeFrame = "week";
+            doughnutChartCenterLabel = "This Week";
+        case "Past Month":
+            dataTimeFrame = "month";
+            doughnutChartCenterLabel = "This Month";
+    }
+    Chart.pluginService.register({
+		beforeDraw: function (chart) {
+			if (chart.config.options.elements.center) {
+        //Get ctx from string
+        var ctx = chart.chart.ctx;
+
+				//Get options from the center object in options
+        var centerConfig = chart.config.options.elements.center;
+      	var fontStyle = centerConfig.fontStyle || 'Arial';
+				var txt = centerConfig.text;
+        var color = centerConfig.color || '#000';
+        var sidePadding = centerConfig.sidePadding || 20;
+        var sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
+        //Start with a base font of 30px
+        ctx.font = "30px " + fontStyle;
+
+				//Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+        var stringWidth = ctx.measureText(txt).width;
+        var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+
+        // Find out how much the font can grow in width.
+        var widthRatio = elementWidth / stringWidth;
+        var newFontSize = Math.floor(30 * widthRatio);
+        var elementHeight = (chart.innerRadius * 2);
+
+        // Pick a new font size so it will not be larger than the height of label.
+        var fontSizeToUse = Math.min(newFontSize, elementHeight);
+
+				//Set font settings to draw it correctly.
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+        var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+        ctx.font = fontSizeToUse+"px " + fontStyle;
+        ctx.fillStyle = color;
+
+        //Draw text in center
+        ctx.fillText(txt, centerX, centerY);
+			}
+		}
+	});
     // retrieve data from CSV file
     $.get(csvFileLocation, function(fileContent) {
         var labelsAndData = sortDataForTotalsDoughnutChart(fileContent);
@@ -60,6 +109,13 @@ function drawDoughnutChart(chartLocationID,csvFileLocation,dataTimePeriod,dataTi
                 },
                 tooltips: {
                     enabled: true
+                },
+                elements: {
+                    center: {
+                        text: doughnutChartCenterLabel,
+                        color: "#000000",
+                        sidePadding: 30 // this is a percentage
+                    }
                 }
             }
         }
