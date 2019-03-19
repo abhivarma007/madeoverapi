c7e8f4f2572ddc42436a1a4e08a96dfe23a0e84a
Matthew Grove <matthew@grove.gq>
2019-03-19 07:39:03 +0000

    Set opacity properly with CSS colour variables

--- js/global.js
+++ js/global.js
@@ -1,5 +1,9 @@
+function getCSSColorVariable(variableName) {
+    return "rgb(" + getComputedStyle(document.body).getPropertyValue(variableName) + ")";
+}
+
 $(document).ready(function() {
-    window.text_color = getComputedStyle(document.body).getPropertyValue('--text-color');
-    window.background_color = getComputedStyle(document.body).getPropertyValue('--background-color');
-    window.theme_color = getComputedStyle(document.body).getPropertyValue('--theme-color');
+    window.text_color = getCSSColorVariable("--text-color");
+    window.background_color = getCSSColorVariable('--background-color');
+    window.theme_color = getCSSColorVariable('--theme-color');
 });
